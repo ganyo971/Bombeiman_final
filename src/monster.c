4c0f6ca277c4d0efe66f25f06bcca77ee5b461e3
ganyo <ganyo_971@hotmail.fr>
2015-02-24 18:47:33 +0100

    fichier monster.c créé mais pas encore fonctionnel

---
+++ src/monster.c
@@ -0,0 +1,152 @@
+/* monster.c
+ *
+ *  Created on: 24 févr. 2015
+ *      Author: fumont
+ */
+#include <SDL/SDL_image.h>
+#include <assert.h>
+
+#include <monster.h>
+#include <sprite.h>
+#include <window.h>
+#include <misc.h>
+#include <constant.h>
+
+struct monster {
+	int x, y;
+	enum direction current_direction;
+};
+
+
+struct monster* monster_init() {
+	struct monster* monster = malloc(sizeof(*monster));
+	if (!monster)
+		error("Memory error");
+
+	monster->current_direction = SOUTH;
+
+	return monster;
+}
+
+void monster_free(struct monster* monster) {
+	assert(monster);
+	free(monster);
+}
+
+int player_get_x(struct player* player) {
+	assert(player != NULL);
+	return player->x;
+}
+
+int player_get_y(struct player* player) {
+	assert(player != NULL);
+	return player->y;
+}
+
+void player_set_current_way(struct player* player, enum direction way) {
+	assert(player);
+	player->current_direction = way;
+}
+
+void player_from_map(struct player* player, struct map* map) {
+	assert(player);
+	assert(map);
+
+	int i, j;
+	for (i = 0; i < map_get_width(map); i++) {
+		for (j = 0; j < map_get_height(map); j++) {
+			if (map_get_cell_type(map, i, j) == CELL_PLAYER) {
+				player->x = i;
+				player->y = j;
+			}
+		}
+	}
+}
+
+static int player_move_aux(struct player* player, struct map* map, int x, int y) {
+
+	if (!map_is_inside(map, x, y))
+		return 0;
+
+	switch (map_get_cell_type(map, x, y)) {
+	case CELL_SCENERY:
+		return 0;    // 0 dc mtnt le jouer ne transperce pas les roches et arbres
+		break;
+
+	case CELL_CASE:
+		return 0;    // idem pour caisses
+		break;
+
+	case CELL_BONUS:
+		break;
+
+	case CELL_GOAL:
+		break;
+
+	case CELL_MONSTER:
+		return 0;
+		break;
+
+	case CELL_PLAYER:
+		break;
+
+	default:
+		break;
+	}
+
+	// Player has moved
+	return 1;
+}
+
+int player_move(struct player* player, struct map* map) {
+	int x = player->x;
+	int y = player->y;
+	int move = 0;
+
+	switch (player->current_direction) {
+	case NORTH:
+		if (player_move_aux(player, map, x, y - 1)) {
+			player->y--;
+			move = 1;
+		}
+		break;
+
+	case SOUTH:
+		if (player_move_aux(player, map, x, y + 1)) {
+			player->y++;
+			move = 1;
+		}
+		break;
+
+	case WEST:
+		if (player_move_aux(player, map, x - 1, y)) {
+			player->x--;
+			move = 1;
+		}
+		break;
+
+	case EAST:
+		if (player_move_aux(player, map, x + 1, y)) {
+			player->x++;
+			move = 1;
+		}
+		break;
+	}
+
+	if (move) {
+		map_set_cell_type(map, x, y, CELL_EMPTY);
+		map_set_cell_type(map, player->x, player->y, CELL_PLAYER);
+	}
+	return move;
+}
+
+void player_display(struct player* player) {
+	assert(player);
+	window_display_image(sprite_get_player(player->current_direction),
+			player->x * SIZE_BLOC, player->y * SIZE_BLOC);
+}
+
+
+
+
+
